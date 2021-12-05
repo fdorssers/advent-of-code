c16e8fc8b5546446648a7203814f335616417408
Frank Dorssers <frank.dorssers@gmail.com>
2021-12-05 18:24:39 +0100

    Reenable commented code in 2021 task 2

--- aoc_2021/src/task02.py
+++ aoc_2021/src/task02.py
@@ -21,17 +21,16 @@ class Command:
 def calculate_movement(commands: List[Command], task_one: bool) -> int:
     depth, depthv2, horizontal, aim = 0, 0, 0, 0
     for command in commands:
-        pass
-        # match command:
-        #     case Command(direction="forward", steps=steps):
-        #         horizontal += steps
-        #         depthv2 += aim * steps
-        #     case Command(direction="down", steps=steps):
-        #         depth += steps
-        #         aim += steps
-        #     case Command(direction="up", steps=steps):
-        #         depth -= steps
-        #         aim -= steps
+        match command:
+            case Command(direction="forward", steps=steps):
+                horizontal += steps
+                depthv2 += aim * steps
+            case Command(direction="down", steps=steps):
+                depth += steps
+                aim += steps
+            case Command(direction="up", steps=steps):
+                depth -= steps
+                aim -= steps
     return (horizontal * depth) if task_one else (horizontal * depthv2)
 
 
