0d998cb675189fc14b21687ebc51a9f0029588f2
Frank Dorssers <frank.dorssers@gmail.com>
2021-12-09 14:28:24 +0100

    Drop dataclass from task 9

--- aoc_2021/src/task09.py
+++ aoc_2021/src/task09.py
@@ -1,9 +1,7 @@
 import math
 from collections import deque
-from dataclasses import dataclass
 from itertools import product
 from typing import Iterator
-from typing import Tuple
 
 import numpy as np
 
@@ -11,40 +9,32 @@ from utils import load_data
 from utils import print_result
 
 
-@dataclass(frozen=True)
-class Point:
-    row: int
-    col: int
-
-    @property
-    def tuple(self) -> Tuple[int, int]:
-        return self.row, self.col
-
-
-def find_minima(array: np.array) -> Iterator[Point]:
+def find_minima(array: np.array) -> Iterator[tuple[int, int]]:
     height, width = array.shape
     for row, col in product(range(height), range(width)):
         if array[max(row - 1, 0) : row + 2, max(col - 1, 0) : col + 2].min() == array[row, col]:
-            yield Point(row, col)
+            yield row, col
 
 
 def part1(array: np.array) -> int:
-    return sum(array[point.tuple] + 1 for point in find_minima(array))
+    return sum(array[point] + 1 for point in find_minima(array))
 
 
-def find_neighbours(point: Point, array: np.array) -> Iterator[Point]:
+def find_neighbours(point: tuple[int, int], array: np.array) -> Iterator[tuple[int, int]]:
     height, width = array.shape
     for col, row in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-        new_point = Point(col=point.col + col, row=point.row + row)
-        if (0 <= new_point.col < width) and (0 <= new_point.row < height):
+        new_point = (point[0] + row, point[1] + col)
+        if (0 <= new_point[1] < width) and (0 <= new_point[0] < height):
             yield new_point
 
 
-def is_part_of_basin(current: Point, neighbour: Point, array: np.array) -> bool:
-    return bool((array[current.tuple] < array[neighbour.tuple]) and (array[neighbour.tuple] < 9))
+def is_part_of_basin(
+    current: tuple[int, int], neighbour: tuple[int, int], array: np.array
+) -> bool:
+    return bool((array[current] < array[neighbour]) and (array[neighbour] < 9))
 
 
-def find_basin(point: Point, array: np.array) -> set[Point]:
+def find_basin(point: tuple[int, int], array: np.array) -> set[tuple[int, int]]:
     basin = {point}
     queue = deque([point])
     while queue:
