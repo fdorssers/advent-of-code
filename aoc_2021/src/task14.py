61f95a54409de3b502ad1a397fc734e05607104b
Frank Dorssers <frank.dorssers@gmail.com>
2021-12-14 10:34:58 +0100

    Finish task 14 from 2021

--- aoc_2021/src/task14.py
+++ aoc_2021/src/task14.py
@@ -14,13 +14,27 @@ def parse_data(lines: list[str]) -> tuple[str, defaultdict[str, str]]:
     return template_group[0], defaultdict(lambda: "", rule_dict)
 
 
+def init_structures(
+    template: str, rules: defaultdict[str, str]
+) -> tuple[defaultdict[str, int], defaultdict[str, int], defaultdict[str, list[str]]]:
+    two_letter_counts = defaultdict(
+        lambda: 0, Counter(["".join(t) for t in zip(template, template[1:])])
+    )
+    single_letter_counts = defaultdict(lambda: 0, Counter(template))
+    rule_dict = defaultdict(list, {k: [f"{k[0]}{v}", f"{v}{k[1]}"] for k, v in rules.items()})
+    return single_letter_counts, two_letter_counts, rule_dict
+
+
 def part(template: str, rules: defaultdict[str, str], steps: int) -> int:
+    single_letter_counts, two_letter_counts, rule_dict = init_structures(template, rules)
     for _ in range(steps):
-        template = "".join(
-            [f"{a}{rules[a+b]}" for a, b in zip(template, template[1:])] + [template[-1]]
-        )
-    counts = Counter(template)
-    return max(counts.values()) - min(counts.values())
+        new_two_letter_counts: defaultdict[str, int] = defaultdict(lambda: 0)
+        for two_letter_word, two_letter_word_occurrences in two_letter_counts.items():
+            single_letter_counts[rules[two_letter_word]] += two_letter_word_occurrences
+            for new_two_letter_word in rule_dict[two_letter_word]:
+                new_two_letter_counts[new_two_letter_word] += two_letter_word_occurrences
+        two_letter_counts = new_two_letter_counts
+    return max(single_letter_counts.values()) - min(single_letter_counts.values())
 
 
 if __name__ == "__main__":
@@ -30,6 +44,5 @@ if __name__ == "__main__":
     assert part(example_template, example_rules, 10) == 1588
     print_result(1, part(template, rules, 10))
 
-    # This one does currently not complete.
     assert part(example_template, example_rules, 40) == 2188189693529
     print_result(1, part(template, rules, 40))
