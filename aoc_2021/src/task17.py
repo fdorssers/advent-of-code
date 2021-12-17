a2020eb6a55b8709a5ff669b89f86086b8019b99
Frank Dorssers <frank.dorssers@gmail.com>
2021-12-17 11:04:56 +0100

    Add part 2 of task 17

--- aoc_2021/src/task17.py
+++ aoc_2021/src/task17.py
@@ -1,13 +1,16 @@
 import re
 from dataclasses import dataclass
+from functools import lru_cache
 from itertools import product
 from typing import Optional
+
+import numpy as np
 
 from utils import load_data
 from utils import print_result
 
 
-@dataclass
+@dataclass(frozen=True)
 class Target:
     start: tuple[int, int]
     end: tuple[int, int]
@@ -28,44 +31,38 @@ def parse_target(line: str) -> Target:
     return Target((start_x, end_y), (end_x, start_y))
 
 
-def add_tuple(t1: tuple[int, int], t2: tuple[int, int]) -> tuple[int, int]:
-    return t1[0] + t2[0], t1[1] + t2[1]
-
-
-def apply_drag(velocity: tuple[int, int]) -> tuple[int, int]:
-    new_y_val = velocity[1] - 1
-    if velocity[0] == 0:
-        return velocity[0], new_y_val
-    elif velocity[0] > 0:
-        return velocity[0] - 1, new_y_val
-    else:
-        return velocity[0] + 1, new_y_val
-
-
-def determine_trajectory(initial_velocity: tuple[int, int], target: Target) -> Optional[int]:
-    current_position = (0, 0)
-    current_velocity = initial_velocity
+def determine_trajectory(init_vel: tuple[int, int], target: Target) -> Optional[int]:
+    cur_pos = (0, 0)
+    cur_vel = init_vel
     highest = 0
-    while (current_velocity[0] > 0) or (current_position[1] >= target.end[1]):
-        current_position = add_tuple(current_position, current_velocity)
-        highest = current_position[1] if current_position[1] > highest else highest
-        if target.in_bounds(current_position):
+    while cur_pos[1] >= target.end[1]:
+        cur_pos = (cur_pos[0] + cur_vel[0], cur_pos[1] + cur_vel[1])
+        highest = cur_pos[1] if cur_pos[1] > highest else highest
+        if target.in_bounds(cur_pos):
             return highest
-        if target.impossible(current_position):
+        if target.impossible(cur_pos):
             return None
-        current_velocity = apply_drag(current_velocity)
+        cur_vel = max(0, cur_vel[0] - 1), cur_vel[1] - 1
     return None
 
 
-def part1(target: Target) -> int:
-    x_velocities = range(0, target.end[0])
-    y_velocities = range(target.end[1], 1000)
-    results = [
+@lru_cache()
+def find_heights_for_all_trajectories(target: Target) -> list[int]:
+    x_velocities = range(0, target.end[0] + 1)
+    y_velocities = range(target.end[1], np.abs(target.end[1]))
+    return [
         max_height
         for velocity in product(x_velocities, y_velocities)
-        if (max_height := determine_trajectory(velocity, target))
+        if (max_height := determine_trajectory(velocity, target)) is not None
     ]
-    return max(results)
+
+
+def part1(target: Target) -> int:
+    return max(find_heights_for_all_trajectories(target))
+
+
+def part2(target: Target) -> int:
+    return len(find_heights_for_all_trajectories(target))
 
 
 if __name__ == "__main__":
@@ -75,5 +72,5 @@ if __name__ == "__main__":
     assert part1(example_data) == 45
     print_result(1, part1(data))
 
-    # assert cheapest_fuel(example_data, p2_calc) == 168
-    # print_result(1, cheapest_fuel(data, p2_calc))
+    assert part2(example_data) == 112
+    print_result(1, part2(data))
