1239e8bb1b27e52679342a562bb1ba20d68aad7d
Frank Dorssers <frank.dorssers@gmail.com>
2023-12-03 16:35:52 +0100

    Add task 2 day 3 AoC 2023

--- aoc_2023/src/day03.py
+++ aoc_2023/src/day03.py
@@ -1,3 +1,6 @@
+import math
+from collections import defaultdict
+
 from utils import load_data
 from utils import print_result
 
@@ -25,40 +28,73 @@ def is_adjacent(x: int, y: int, data: list[str]) -> bool:
     return False
 
 
-def task01(data: list[str]) -> int:
+def is_adjacent_to_star(x: int, y: int, data: list[str]) -> set[tuple[int, int]]:
+    gear_coords = set()
+    y_diff = (-1, 0, 1)
+    x_diff = (-1, 0, 1)
+    for x_d in x_diff:
+        for y_d in y_diff:
+            if x_d == 0 and y_d == 0:
+                continue
+            x_new = x + x_d
+            y_new = y + y_d
+            if (x_new < 0) or (y_new < 0):
+                continue
+            if (x_new >= len(data[y])) or (y_new >= len(data)):
+                continue
+
+            if data[y_new][x_new] == "*":
+                gear_coords.add((x_new, y_new))
+
+    return gear_coords
+
+
+def task(data: list[str], task: int) -> int:
     chars = []
     adjacent_chars = []
+
+    gears: dict[tuple[int, int], list[int]] = defaultdict(list)
+
     for y, line in enumerate(data):
         cur_char = ""
         cur_char_adjacent = False
+
+        cur_gear_coords = set()
+
         for x, char in enumerate(line):
             if char.isdigit():
                 cur_char += char
                 cur_char_adjacent |= is_adjacent(x, y, data)
+                cur_gear_coords.update(is_adjacent_to_star(x, y, data))
             else:
                 if cur_char:
                     chars.append(int(cur_char))
                     if cur_char_adjacent:
                         adjacent_chars.append(int(cur_char))
+                        for cgc in cur_gear_coords:
+                            gears[cgc] += [int(cur_char)]
                     cur_char = ""
                     cur_char_adjacent = False
+                    cur_gear_coords = set()
         if cur_char:
             chars.append(int(cur_char))
             if cur_char_adjacent:
                 adjacent_chars.append(int(cur_char))
-    return sum(adjacent_chars)
-
-
-def task02(data: list[list[dict[str, int]]]) -> int:
-    return 0
+                for cgc in cur_gear_coords:
+                    gears[cgc] += [int(cur_char)]
+    gear_ratios = []
+    for gear, nums in gears.items():
+        if len(nums) > 1:
+            gear_ratios.append(math.prod(nums))
+    return sum(adjacent_chars) if task == 1 else sum(gear_ratios)
 
 
 if __name__ == "__main__":
     example_data = load_data(True)
     data = load_data()
 
-    assert task01(example_data) == 4361
-    print_result(1, task01(data))
-    #
-    # assert task02(example_data) == 2286
-    # print_result(2, task02(data))
+    assert task(example_data, 1) == 4361
+    print_result(1, task(data, 1))
+
+    assert task(example_data, 2) == 467835
+    print_result(2, task(data, 2))
