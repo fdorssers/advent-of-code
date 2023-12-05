e9cf1aeda179fa6935597555d3890ad89a114148
Frank Dorssers <frank.dorssers@gmail.com>
2023-12-05 15:20:44 +0100

    Update day 5 AoC 2023

--- aoc_2023/src/day05.py
+++ aoc_2023/src/day05.py
@@ -22,103 +22,86 @@ class SourceDestinationMap:
         return SourceDestinationMap(*map(int, line.strip().split()))
 
 
-@dataclass
-class AlmanacMap:
-    from_: str
-    to: str
-    source_destination_maps: list[SourceDestinationMap]
-
-    @staticmethod
-    def parse(lines: list[str]) -> AlmanacMap:
-        from_, to = lines[0].split()[0].split("-to-")
-        maps = [SourceDestinationMap.parse(line) for line in lines[1:]]
-        return AlmanacMap(from_, to, maps)
-
-    def convert_number(self, number: int) -> int:
-        for sdm in self.source_destination_maps:
-            if sdm.source_range_start <= number < sdm.source_range_start + sdm.range_length:
-                return number - sdm.source_range_start + sdm.destination_range_start
-        return number
-
-    def convert_number_range(self, number_range: tuple[int, int]) -> list[tuple[int, int]]:
-        # print(f"    Converting {number_range=}")
-        queue = deque([number_range])
-        result = []
-        while queue:
-            #             print(f"      Current queue: {queue}")
-            current = queue.popleft()
-            found_match = False
-            for sdm in self.source_destination_maps:
-                if not (
-                    sdm.source_range_start
-                    <= current[0]
-                    < sdm.source_range_start + sdm.range_length
-                ):
-                    #                     print(
-                    #                         f"        ❌ {current=} not in {sdm.source_range_start=}+{sdm.range_length=} (={sdm.source_range_start+sdm.range_length})"
-                    #                     )
-                    continue
-                #                 print(
-                #                     f"        ✅ {current=} in {sdm.source_range_start=}+{sdm.range_length=} (={sdm.source_range_start+sdm.range_length})"
-                #                 )
-                source_start = sdm.source_range_start
-                source_length = sdm.range_length
-                source_end = source_start + source_length
-
-                destination_start = sdm.destination_range_start
-
-                current_start = current[0]
-                current_length = current[1]
-                current_end = current[0] + current_length
-
-                new_start = current_start - source_start + destination_start
-                new_length = min(current_length, source_end - current_start)
-
-                #                 print(f"          Adding {(new_start, new_length)} to result")
-                result.append((new_start, new_length))
-                remaining_length = current_length - new_length
-                if remaining_length:
-                    #                     print(
-                    #                         f"          Has remaining length {remaining_length}, adding to queue: {(current_start + new_length, remaining_length)=}"
-                    #                     )
-                    queue.append((current_start + new_length, remaining_length))
-                found_match = True
-                break
-            if not found_match:
-                result.append(current)
-        return result
-
-    def convert_number_ranges(self, number_ranges: list[tuple[int, int]]) -> list[tuple[int, int]]:
-        #         print(f"  Converting number ranges: {number_ranges}")
-        new_ranges = []
-        for number_range in number_ranges:
-            number_range_result = self.convert_number_range(number_range)
-            #             print(f"    {number_range=} ==> {number_range_result=}")
-            new_ranges.extend(number_range_result)
-        return new_ranges
-
-    def convert_numbers(self, numbers: list[int]) -> list[int]:
-        return [self.convert_number(number) for number in numbers]
+def parse_map_lines(lines: list[str]) -> list[SourceDestinationMap]:
+    return [SourceDestinationMap.parse(line) for line in lines[1:]]
 
 
-def parse(data: list[str]) -> tuple[list[int], dict[str, AlmanacMap]]:
+def parse(data: list[str]) -> tuple[list[int], list[list[SourceDestinationMap]]]:
     seeds = list(map(int, data[0].split(": ")[1].split()))
     almanac_maps = [
-        AlmanacMap.parse(list(g)) for key, g in groupby(data[1:], lambda line: not line) if not key
+        parse_map_lines(list(g)) for key, g in groupby(data[1:], lambda line: not line) if not key
     ]
-    return seeds, {almanac_map.from_: almanac_map for almanac_map in almanac_maps}
+    return seeds, almanac_maps
 
 
-def task01(seeds: list[int], almanac: dict[str, AlmanacMap]) -> int:
-    return min(reduce(lambda a, b: b.convert_numbers(list(a)), almanac.values(), seeds))
+##########
+# Task 1 #
+##########
 
 
-def task02(seeds: list[int], almanac: dict[str, AlmanacMap]) -> int:
+def convert_number(number: int, sdms: list[SourceDestinationMap]) -> int:
+    for sdm in sdms:
+        if sdm.source_range_start <= number < sdm.source_range_start + sdm.range_length:
+            return number - sdm.source_range_start + sdm.destination_range_start
+    return number
+
+
+def convert_numbers(numbers: list[int], sdms: list[SourceDestinationMap]) -> list[int]:
+    return [convert_number(number, sdms) for number in numbers]
+
+
+def task01(seeds: list[int], almanac: list[list[SourceDestinationMap]]) -> int:
+    return min(reduce(lambda a, am: convert_numbers(list(a), am), almanac, seeds))
+
+
+##########
+# Task 2 #
+##########
+
+
+def convert_number_range(
+    number_range: tuple[int, int], sdms: list[SourceDestinationMap]
+) -> list[tuple[int, int]]:
+    queue = deque([number_range])
+    result = []
+    while queue:
+        current = queue.popleft()
+        for sdm in sdms:
+            if not (
+                sdm.source_range_start <= current[0] < sdm.source_range_start + sdm.range_length
+            ):
+                continue
+
+            new_length = min(current[1], sdm.source_range_start + sdm.range_length - current[0])
+
+            result.append(
+                (
+                    current[0] - sdm.source_range_start + sdm.destination_range_start,
+                    new_length,
+                )
+            )
+            remaining_length = current[1] - new_length
+            if remaining_length:
+                queue.append((current[0] + new_length, remaining_length))
+            break
+        else:
+            result.append(current)
+    return result
+
+
+def convert_number_ranges(
+    number_ranges: list[tuple[int, int]], sdms: list[SourceDestinationMap]
+) -> list[tuple[int, int]]:
+    new_ranges = []
+    for number_range in number_ranges:
+        new_ranges.extend(convert_number_range(number_range, sdms))
+    return new_ranges
+
+
+def task02(seeds: list[int], almanac: list[list[SourceDestinationMap]]) -> int:
     number_ranges = list(map(tuple, chunked(seeds, 2)))
-    for am in almanac.values():
-        #         print(f"📖 Processing {am.from_} ==> {am}")
-        number_ranges = am.convert_number_ranges(number_ranges)
-    #     print(min(nr[0] for nr in number_ranges))
+    for am in almanac:
+        number_ranges = convert_number_ranges(number_ranges, am)
     return int(min(nr[0] for nr in number_ranges))
 
 
